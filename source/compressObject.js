a2611e76fec4574e7b35a76f1edae7bc6f1c40b1
kertoflenida <ACITONXLEB@gmail.com>
2025-09-08 23:09:12 +0300

    Исправление ошибок проверки качества кода

--- source/compressObject.js
+++ source/compressObject.js
@@ -11,16 +11,16 @@
  * @returns {Object} newObject - новый объект без null, undefined, "". 
  */
 const compressObject = function (obj) {
-    if (typeof(obj) !== "object" || typeof(obj) === null || Array.isArray(obj)){
+    if (obj === null || typeof(obj) !== "object" || Array.isArray(obj)){
         throw new TypeError('Это не объект!')   
     }   
     let newObj = {}
     for (const key in obj){
         const val = obj[key]
         if (val !== null && val !==undefined && val !== ""){
-            if (typeof(val) == "object"){
+            if (typeof(val) === "object"){
                 newObj[key] = compressObject(val)
-            } else if (obj.hasOwnProperty(key)){
+            } else if (Object.prototype.hasOwnProperty.call(obj, key)){
                 newObj[key] = val
             }
         }
