3b175861febfd63ff4f8daf01405ad9eaca5e97c
kertoflenida <ACITONXLEB@gmail.com>
2025-09-10 01:22:10 +0300

    Переписанный вариант через reduce

--- source/compressObject.js
+++ source/compressObject.js
@@ -14,9 +14,7 @@ const compressObject = function (obj) {
     if (obj === null || typeof(obj) !== "object" || Array.isArray(obj)){
         throw new TypeError('Это не объект!')   
     }   
-    let newObj = {}
-    for (const key in obj){
-        const val = obj[key]
+    return Object.entries(obj).reduce((newObj, [key, val]) => {
         if (val !== null && val !==undefined && val !== ""){
             if (typeof(val) === "object"){
                 newObj[key] = compressObject(val)
@@ -24,6 +22,6 @@ const compressObject = function (obj) {
                 newObj[key] = val
             }
         }
-    }
-    return newObj
+        return newObj
+    }, {})
 }
